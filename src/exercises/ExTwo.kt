7b2b214d37d204fce44fbd9bfd0d748fd298597f
Igor <gigor497@gmail.com>
2026-03-18 11:55:49 -0300

    Second exercise is done

--- src/exercises/ExTwo.kt
+++ src/exercises/ExTwo.kt
@@ -3,7 +3,7 @@ package exercises
 fun main(){
 
     val name: String = "João"
-    val age: Int = 25
+    var age: Int = 25
     val height: Double = 1.75
 
     println("Meu nome é $name tenho $age anos de idade e $height m de altura")
