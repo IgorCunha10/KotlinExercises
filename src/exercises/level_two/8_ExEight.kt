a4eae8baa3de3686061a7cfad712e933369198b3
Igor <gigor497@gmail.com>
2026-03-19 15:55:21 -0300

    Eight exercise done

--- src/exercises/level_two/8_ExEight.kt
+++ src/exercises/level_two/8_ExEight.kt
@@ -1,2 +1,33 @@
 package exercises.level_two
 
+fun main() {
+
+    println("Digite um número para checar se ele é primo:")
+
+    val num = readln()
+    val numConv = num.toInt()
+
+    var i = 2
+    var isPrime = true
+
+    if(numConv <= 1) {
+        isPrime = false
+    } else {
+        while(i * i <= numConv) {
+            if(numConv % i == 0) {
+                isPrime = false
+
+            }
+            i++
+        }
+
+    }
+
+    if (isPrime) {
+        println("O número enviado é primo")
+    } else {
+        println("O número enviado não é primo")
+    }
+
+
+}
