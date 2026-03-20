7e8df61b51800db4c667121a5ea9f38871a16c8f
Igor <gigor497@gmail.com>
2026-03-20 10:56:42 -0300

    Exercise number nine is done

--- src/exercises/level_two/9_ExEight.kt
+++ src/exercises/level_two/9_ExEight.kt
@@ -1,2 +1,22 @@
 package exercises.level_two
 
+fun main() {
+
+    println("Digite um número para calcular o seu fatorial: ")
+    val num = readln()
+    var numConv = num.toInt()
+    var fatorial = 1
+
+    if(numConv < 0 ) {
+        println("Não foi possível calcular o fatorial!")
+    } else if(numConv == 0 ) {
+        println("O fatorial de 0 é: 1")
+    } else {
+        while (numConv > 0) {
+            fatorial = fatorial * numConv
+            numConv--
+        }
+        println("O fatorial de " + num + " é: " + fatorial)
+    }
+
+    }
