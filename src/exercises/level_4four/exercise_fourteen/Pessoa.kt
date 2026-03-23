400940d2a571bbc3f853b093189cc0f02d764b0b
Igor <gigor497@gmail.com>
2026-03-23 15:54:01 -0300

    Finished exercise Fourteen

---
+++ src/exercises/level_4four/exercise_fourteen/Pessoa.kt
@@ -0,0 +1,5 @@
+package exercises.level_4four.exercise_fourteen
+
+class Pessoa(val nome: String, var idade: Int){
+
+}
---
+++ src/exercises/level_4four/exercise_fourteen/PessoaObj.kt
@@ -0,0 +1,23 @@
+package exercises.level_4four.exercise_fourteen
+
+
+fun main() {
+
+    var pessoaA = Pessoa("Jurandir", 60)
+    var pessoaB = Pessoa("Enzo", 12)
+
+    var diferencaIdade: Int
+
+    if(pessoaA.idade > pessoaB.idade) {
+        diferencaIdade = pessoaA.idade - pessoaB.idade
+        println("${pessoaA.nome} é a pessoa mais velha")
+        println("A diferença de idade entre as pessoas é de $diferencaIdade anos")
+    } else if(pessoaB.idade > pessoaA.idade) {
+        diferencaIdade = pessoaB.idade - pessoaA.idade
+        println("${pessoaB.nome} é a pessoa mais velha")
+        println("A diferença de idade entre as duas pessoas é de $diferencaIdade anos")
+    } else {
+        println("A idade das duas pessoas é a mesma")
+    }
+
+}
