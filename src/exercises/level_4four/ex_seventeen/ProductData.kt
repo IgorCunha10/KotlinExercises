8daa3224d70b8c959a9dc97e1f8494fa3ae44173
Igor <gigor497@gmail.com>
2026-03-24 15:55:57 -0300

    Exercise seventeen done

---
+++ src/exercises/level_4four/ex_seventeen/ProductData.kt
@@ -0,0 +1,5 @@
+package exercises.level_4four.ex_seventeen
+
+data class ProductData(val name:String, val price:Int) {
+
+}
---
+++ src/exercises/level_4four/ex_seventeen/productMain.kt
@@ -0,0 +1,7 @@
+package exercises.level_4four.ex_seventeen
+
+fun main(){
+    val product = ProductData("Chocolate", 10)
+    println(product.name)
+    println(product.price)
+}
