f02b0107995912c0a4fb8fb741deb47cf89dc07a
Igor <gigor497@gmail.com>
2026-03-24 11:33:07 -0300

    Created the Conta class and the methods related to a bank account

--- src/exercises/level_4four/level_fifteen/Conta.kt
+++ src/exercises/level_4four/level_fifteen/Conta.kt
@@ -1,7 +1,29 @@
 package exercises.level_4four.level_fifteen
 
-class Conta {
+class Conta(val nome: String, var saldo: Double) {
 
+    var valor: Double = 0.0
 
+    fun depositar() {
+        println("Digite o valor a ser depositado:")
+        valor = readln().toDouble()
+        if (valor < 0) {
+            println("Valor inválido")
+        } else {
+            saldo += valor
+        }
+    }
 
+    fun sacar() {
+        println("O valor disponível para saque é ${saldo}")
+        println("Digite o valor que deseja sacar: ")
+        valor = readln().toDouble()
+
+        if(valor > saldo || valor < 0){
+            println("Valor inválido")
+        } else {
+            saldo -= valor
+        }
+        println("Operação finalizada.")
+    }
 }
