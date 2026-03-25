535d3310a339df46f5868b34e7d749d0090391b4
Igor <gigor497@gmail.com>
2026-03-25 12:50:53 -0300

    Updated the solution of twenty exercise

--- src/exercises/level_5five/ex_twenty/20_ExTwenty.kt
+++ src/exercises/level_5five/ex_twenty/20_ExTwenty.kt
@@ -4,11 +4,17 @@ fun main() {
 
     println("Digite um lista de números")
 
-    val numsUsuario = readln().split(' ').map { it.toInt() }
+    val numsUsuario = readln().split(' ').map { it.toDouble() }
     println(numsUsuario)
 
     val doubleNums = numsUsuario.map { it * 2 }
     println("O dobro dos números digitados é: $doubleNums")
 
+    val sumNums = numsUsuario.reduce { accumulator, element ->
+        accumulator + element
+    }
 
+    val sumNumsFormatted = "%.2f".format(sumNums)
+
+    println("A soma dos números enviados é: $sumNumsFormatted")
 }
