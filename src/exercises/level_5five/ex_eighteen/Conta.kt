e534d71c30c0e6a99afa349fe0432c17a8f6a820
Igor <gigor497@gmail.com>
2026-03-25 10:28:52 -0300

    Finished exercise eighteen

---
+++ src/exercises/level_5five/ex_eighteen/Conta.kt
@@ -0,0 +1,6 @@
+package exercises.level_5five.ex_eighteen
+
+class Conta(var usuario: String, var senha: String) {
+
+
+}
---
+++ src/exercises/level_5five/ex_eighteen/SisLogin.kt
@@ -0,0 +1,31 @@
+package exercises.level_5five.ex_eighteen
+
+fun main() {
+
+   println("Digite o usuário e senha para criação da conta")
+
+    val contaUsuario = Conta(readln().toString(),
+        readln().toString())
+
+    println("Conta Criada com sucesso!")
+
+    println("Digite agora o usuário para efetuar o login no sistema: ")
+    var loginUsuario = readln().toString()
+
+    println("Digite a senha para efetuar o login no sistema: ")
+    var senhaUsuario = readln().toString()
+
+    if (loginUsuario != contaUsuario.usuario || contaUsuario.senha != senhaUsuario) {
+        println("Credenciais inválidas. Tente novamente mais tarde!")
+    } else {
+        println("Login efetuado com sucesso!")
+    }
+
+}
+
+
+
+
+
+
+
