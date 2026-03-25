b3aae32721d57a53e53ab08fb770dc18f0a1421a
Igor <gigor497@gmail.com>
2026-03-25 12:02:20 -0300

    Finished the exercise number eighteen

--- src/exercises/level_5five/ex_eighteen/SisLogin.kt
+++ src/exercises/level_5five/ex_eighteen/SisLogin.kt
@@ -2,7 +2,7 @@ package exercises.level_5five.ex_eighteen
 
 fun main() {
 
-   println("Digite o usuário e senha para criação da conta")
+   println("Digite o usuário e senha para criação da conta: ")
 
     val contaUsuario = Conta(readln().toString(),
         readln().toString())
