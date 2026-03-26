a403a9e0be8532a678c53a15bcee84b6b9c12da3
Igor <gigor497@gmail.com>
2026-03-26 12:53:28 -0300

    Solved exercise twenty four

--- src/exercises/level_6six/ex_twentyfour/24_ExTwentyFour.kt
+++ src/exercises/level_6six/ex_twentyfour/24_ExTwentyFour.kt
@@ -1,2 +1,20 @@
 package exercises.level_6six.ex_twentyfour
 
+suspend fun main() {
+    showUserInfo()
+}
+
+suspend fun showUserInfo() {
+    println("Loading information...")
+    greet()
+//    delay(1L)
+    println("User: Igor Cunha")
+}
+
+suspend fun greet() {
+    println("Hello!")
+}
+
+suspend fun delay(timeMillis: Long) {
+
+}
