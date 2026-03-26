76545a92200bc66b5f96f870d3f54f55ed1ef05e
Igor <gigor497@gmail.com>
2026-03-26 10:37:05 -0300

    Updated exercise twenty two

--- src/exercises/level_5five/ex_twentytwo/CountLines.kt
+++ src/exercises/level_5five/ex_twentytwo/CountLines.kt
@@ -4,13 +4,8 @@ import java.io.File
 
 class CountLines {
 
-//    fun countLines(filepath: String): Int {
-//        return File(filepath).useLines { it.count() }
-//
-//    }
-
     fun countLines(filepath: String): Int {
-        println("O número de linhas no arquivo é: ")
+        println("O número de linhas no arquivo enviado é:")
         return File(filepath).useLines { it.count() }
     }
 
