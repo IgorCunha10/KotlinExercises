e02593fe20e6419f1ef50d87cac82e4d50609fc6
Igor <gigor497@gmail.com>
2026-03-31 10:47:03 -0300

    Created a simple authentication system

--- src/exercises/level_6six/ex_twentysix/Sistema.kt
+++ src/exercises/level_6six/ex_twentysix/Sistema.kt
@@ -1,30 +1,56 @@
 package exercises.level_6six.ex_twentysix
 
+import kotlin.math.log
+
 private var escolha: Boolean = false
 private var total: Double = 0.0
+private var user: String = ""
+private var senha: String = ""
 
 fun main(){
     registerUser()
     println("")
 
-    do {
-        showMenu()
-        getMenuChoice()
-    }while (escolha)
+   if (login()) {
+       do {
+           showMenu()
+           getMenuChoice()
+       } while (escolha)
 
-    calcularTotal()
-
+       calcularTotal()
+   }
 }
 
 fun registerUser() {
     println("Olá, Digite o seu nome para se cadastrar no sistema do nosso restaurante.")
-    val user = readln()
+    user = readln()
     println("Digite agora a senha da sua conta:")
-    val senha = readln()
+    senha = readln()
     println("Conta criada com sucesso!")
 
 }
 
+fun login(): Boolean {
+    var sucesso: Boolean
+
+    println("Digite suas credenciais para efetuar o login na plataforma")
+    println("USUÁRIO:")
+    val userLogin = readln()
+    println("SENHA:")
+    val userSenha = readln()
+
+    if (userLogin != user) {
+        println("Credencial Inválida")
+        sucesso = false
+    } else if(userSenha != senha) {
+        println("Senha Inválida")
+        sucesso = false
+    } else {
+        sucesso = true
+    }
+
+    return sucesso
+}
 fun getMenuChoice() {
     var userEscolha = readln().toInt()
     if (userEscolha <= 3) {
@@ -49,12 +75,13 @@ fun showMenu() {
     println("Bem vindo ao nosso restaurante!")
     println("Digite o que deseja:")
     println("1 - Cardápio")
-    println("2 - Finalizar Compra")
-    println("3 - Ver valor do carrinho")
+    println("2 - Ver valor do carrinho")
+    println("3 - Finalizar Compra")
     println("4 - Sair")
 }
 
 fun showCarte(): Int {
+    println("--CARDÁPIO--")
     println("1 - Macarrão à bolonhesa - R$20,00")
     println("2 - Lasagna - R$30,00")
     println("3 - Escondidinho de carne - R$35,00")
